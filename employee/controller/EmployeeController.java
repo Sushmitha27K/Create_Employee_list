717e0ada90732176f2c5622143f53cb65559a1f7
Sushmitha27K <sushmitha2721.k@gmail.com>
2024-07-16 16:36:15 +0530

    Add files via upload

---
+++ employee/controller/EmployeeController.java
@@ -0,0 +1,29 @@
+package com.example.employee.controller;
+
+import com.example.employee.model.Employee;
+import com.example.employee.service.EmployeeService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+
+    @PostMapping
+    public Employee saveEmployee(@RequestBody Employee employee) {
+        return employeeService.saveEmployee(employee);
+    }
+}
